ddab9424a1c09e08d8094a1a0bb1a58f6554e630
daief48 <daief20103048@gmail.com>
2023-03-31 02:53:41 +0600

    Commit 2 test

--- practice1/practice1/views.py
+++ practice1/practice1/views.py
@@ -3,22 +3,39 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    params = {'name':'Daief','place':'USA'}
+    return render(request, 'index.html',params)
 
     # return HttpResponse("Home")
 
+def analyze(request):
+    djtext = request.GET.get('text','default')
+    removepunc = request.GET.get('removepunc','off')
+    print(djtext)
+    punctuations = '''!()-{}[];:;"\,<>./?@#$^&*_~%'''
+    analyzed = ""
+    print(removepunc)
+    if removepunc == 'off':
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctions','analyzed':analyzed}
+        return render(request,'analyze.html',params)
+    else:
+        return HttpResponse("Error")
 def removepunc(request):
+    print(request.GET.get('text','default'))
     return HttpResponse("remove punc")
 
-def capfirst(request):
-    return HttpResponse("capitalize first")
+# def capfirst(request):
+#     return HttpResponse("capitalize first")
 
-def newlineremove(request):
-    return HttpResponse("newline remove first")
+# def newlineremove(request):
+#     return HttpResponse("newline remove first")
 
 
-def spaceremove(request):
-    return HttpResponse("space remover back")
+# def spaceremove(request):
+#     return HttpResponse("space remover back")
 
-def charcount(request):
-    return HttpResponse("charcount ")
+# def charcount(request):
+#     return HttpResponse("charcount ")
